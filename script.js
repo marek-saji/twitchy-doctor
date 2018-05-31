233bf5076a91000a0a24f388ca5dfba890369617
Glitch (twitchy-doctor) <none>
2018-05-31 15:54:56 +0000

    👩🐈 Checkpoint ./script.js:1234503/944

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
-const scheduleTsvSoure = 
-const scheduleTsv = sched.textContent;
+const scheduleTsvSource = document.getElementById('schedule-raw');
+const scheduleTsv = scheduleTsvSource.textContent;
+const schedule = document.createElement('ol');
 
+scheduleTsvSource.style.display = 'none';
+document.body.appendChild(schedule);
+
+schedule.innerHTML = JSON.stringify(scheduleTsv.split("\n").map(line => {
+  const row = line.split("\t");
+  const date = new Date(row[0].split(' ').reverse().join(' '));
+  row[0] = date;
+  console.log(row[1]);
+  row[1] = row[1].split(/ *, +/);
+  
+  for (let i = 2; i <= 4; i += 1)
+  {
+  }
+  return row;
+}));
