94fbc689e069498aec3499bdd7167014c3ca687d
Glitch (twitchy-doctor) <none>
2018-06-01 14:59:45 +0000

    🚄🌨 Checkpoint ./service-worker.js:1234503/670 ./index.html:1234503/176

---
+++ service-worker.js
@@ -0,0 +1,19 @@
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open('complete').then(function(cache) {
+      return cache.addAll([
+        '/style.css',
+        '/script.js',
+        '/index.html',
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    fetch(event.request).catch(function() {
+      return caches.match(event.request);
+    })
+  );
+});
